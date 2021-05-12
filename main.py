1834c9a0c1294f5fcc1f811dec65deb86c72c31f
Rui Pereira <ruisilvapereira1@gmail.com>
2021-05-12 22:20:52 +0100

    CV actualizado

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/aboutme/")
 def about_me():
-    return render_template("about me.html")
+    return render_template("about_me.html")
 
 @app.route("/portefolio/")
 def portfolio():
@@ -17,6 +17,18 @@ def portfolio():
 @app.route("/portefolio/fakebook/")
 def fakebook():
     return render_template("Fakebook.html")
+
+@app.route("/portefolio/boogle/")
+def boogle():
+    return render_template("boogle.html")
+
+@app.route("/portefolio/boogle/login")
+def login_boogle():
+    return render_template("Login.html")
+
+@app.route("/portefolio/cabeleireiro/")
+def cabeleireiro():
+    return render_template("cabeleireiro.html")
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
