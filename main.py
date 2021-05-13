5e0350df908dd76103c404347b10b2076de683d1
Rui Pereira <ruisilvapereira1@gmail.com>
2021-05-13 19:20:29 +0100

    teste

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
+#from flask import Flask, render_template
+
+#app = Flask(__name__)
+
+#@app.route("/")
+#def index():
+#    return render_template("index.html")
+
+#@app.route("/aboutme/")
+#def about_me():
+#    return render_template("about_me.html")
+
+#@app.route("/portefolio/")
+#def portfolio():
+#    return render_template("portefolio.html")
+
+#@app.route("/portefolio/fakebook/")
+#def fakebook():
+#    return render_template("Fakebook.html")
+
+#@app.route("/portefolio/boogle/")
+#def boogle():
+#    return render_template("boogle.html")
+
+#@app.route("/portefolio/boogle/login/")
+#def login():
+#    return render_template("Login.html")
+
+#@app.route("/portefolio/cabeleireiro/")
+#def cabeleireiro():
+#    return render_template("cabeleireiro.html")
+
+#if __name__ == '__main__':
+#    app.run()
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/aboutme/")
-def about_me():
-    return render_template("about_me.html")
-
-@app.route("/portefolio/")
-def portfolio():
-    return render_template("portefolio.html")
-
-@app.route("/portefolio/fakebook/")
-def fakebook():
-    return render_template("Fakebook.html")
-
-@app.route("/portefolio/boogle/")
-def boogle():
-    return render_template("boogle.html")
-
-@app.route("/portefolio/boogle/login/")
-def login():
-    return render_template("Login.html")
-
-@app.route("/portefolio/cabeleireiro/")
-def cabeleireiro():
-    return render_template("cabeleireiro.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run()  # if you use the port parameter, delete it before deploying to Heroku
